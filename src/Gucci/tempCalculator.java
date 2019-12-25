fdf465836854739c0728b3c18b6bda787fdfdfaf
Rovshan Babayev <ryanbey@Rovshans-MBP.fios-router.home>
2019-12-25 01:26:03 -0500

    adding temperature calculator

---
+++ src/Gucci/tempCalculator.java
@@ -0,0 +1,12 @@
+package Gucci;
+
+public class TempCal {
+    public static void main(String[] args) {
+        double fah = 68; //0c, 273.115k
+
+        //Calculate celcius and store in celcues variable
+        double celsius = (fah - 32) * 5/9;
+        System.out.println(celsius);
+
+    }
+}
